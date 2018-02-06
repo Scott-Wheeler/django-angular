265ea8dc565c391f30af75e435f7c295fe621ef5
Scott Wheeler <me@scottwheeler.ca>
2018-02-05 20:45:27 -0500

    Customize the admin for blog #24

--- django_angular/blog/admin.py
+++ django_angular/blog/admin.py
@@ -3,6 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import Blog, BlogEntry, Author
 
-admin.site.register(Blog)
-admin.site.register(BlogEntry)
+class BlogEntryInline(admin.StackedInline):
+    model = BlogEntry
+    extra = 1
+
+
+class BlogAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["name", "tagline"]})
+    ]
+
+    inlines = [BlogEntryInline]
+
+    list_display = ["name", "tagline"]
+#     list_filter = ["blogentry_set__pub_date"]
+    search_fields = ["name", "tagline"]
+    
+
+admin.site.register(Blog, BlogAdmin)
+# admin.site.register(Blog)
+# admin.site.register(BlogEntry)
+
+
 admin.site.register(Author)
